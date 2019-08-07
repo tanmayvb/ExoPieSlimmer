4be823a18ea63784d7900dd7061687d82fc83f59
Raman Khurana <raman.khurana@cern.ch>
2019-08-07 13:09:22 +0200

    preparing template for Praveen

--- Template.py
+++ Template.py
@@ -10,8 +10,13 @@ def getptetabin(pt,eta):
     return [iptbin,ietabin]
 
 
+def getKey(iptbin,ietabin):
+    ptetakey='pt_'+str(iptbin)+'eta_'+str(ietabin)
+
 def readEleTrigSF(pt, eta):
+    
     iptetabin=getptbin(pt,eta)
     iptbin = iptetabin[0]
     ietabin = iptetabin[1]
     
+    getKey(iptbin,ietabin)
